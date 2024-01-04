9436cf96adc6b2f2b585a2054d2ccee8987cc008
bob <marcellinus.cg@gmail.com>
2024-01-04 12:53:33 +0700

    first

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,15 +1,15 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URL = process.env.MONGODB_URL;
+const MONGODB_URI = process.env.MONGODB_URL;
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
     if (cached.conn) return cached.conn;
 
-    if(!MONGODB_URL) throw new Error('MONGODB_RUL is missing');
+    if(!MONGODB_URI) throw new Error('MONGODB_URL is missing');
 
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URL, {
+    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
         dbName: 'arthur',
         bufferCommands: false,
     })
